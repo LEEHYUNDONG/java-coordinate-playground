0d1ae262a1aee27b971a4559b897c42350039cbe
LEEHYUNDONG <l.steve10@yahoo.com>
2024-09-09 22:59:02 +0900

    feat : 요구사항 1 완료

--- src/main/java/abstract1/Avante.java
+++ src/main/java/abstract1/Avante.java
@@ -1,8 +1,26 @@
 package abstract1;
 
 public class Avante extends Car {
+    private String name;
+    private int tripDistance;
 
-    public Avante(int speed) {
-        super(speed);
+    public Avante(int tripDistance) {
+        name = "Avante";
+        this.tripDistance = tripDistance;
+    }
+
+    @Override
+    double getDistancePerLiter() {
+        return 15;
+    }
+
+    @Override
+    double getTripDistance() {
+        return this.tripDistance;
+    }
+
+    @Override
+    String getName() {
+        return this.name;
     }
 }
--- src/main/java/abstract1/Sonata.java
+++ src/main/java/abstract1/Sonata.java
@@ -1,7 +1,27 @@
 package abstract1;
 
 public class Sonata extends Car {
-    public Sonata(int speed) {
-        super(speed);
+    private String name;
+    private int tripDistance;
+    private int engineEfficiency;
+
+    public Sonata(int tripDistance) {
+        this.name = "Sonata";
+        this.tripDistance = tripDistance;
+    }
+
+    @Override
+    double getDistancePerLiter() {
+        return 10;
+    }
+
+    @Override
+    double getTripDistance() {
+        return tripDistance;
+    }
+
+    @Override
+    String getName() {
+        return "Sonata";
     }
 }
